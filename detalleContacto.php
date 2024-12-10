3aae2881fc071fe68cc9ae2f4c1a07d7271f3aa6
DanielGarciaCampoy <daniel.garcia.campoy@gmail.com>
2024-12-10 20:34:35 +0100

    detalles contacto creado

---
+++ detalleContacto.php
@@ -0,0 +1,47 @@
+<?php
+require_once 'contactosService.php';
+
+// Verificamos que el método sea POST y que el ID esté presente
+if ($_SERVER['REQUEST_METHOD'] !== 'POST' || !isset($_POST['id'])) {
+    header('Location: index.php'); // Redirigimos si no se proporcionó el ID
+    exit();
+}
+
+$id = intval($_POST['id']); // Convertimos el ID a entero para mayor seguridad
+$contacto = getContactoById($id);
+
+if (!$contacto) {
+    echo "<h2>Contacto no encontrado</h2>";
+    echo "<a href='index.php'>Volver a la lista de contactos</a>";
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Detalle del Contacto</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+    <header class="index">
+        <div class="logo">
+            <img src="img/user.jpg" alt="">
+            <h2><?= $contacto['nombre'] . ' ' . $contacto['apellidos'] . ', ' . $contacto['telefono'];;?></h2>
+        </div>
+        <div class="botones">
+            <a href="index.php" class="usuario" title="Volver">
+                <img src="img/volver.png" alt="">
+            </a>
+        </div>
+    </header>
+    <main>
+        
+    </main>
+</body>
+
+</html>
