7d517e88574fdbeeac6955ecd3c16720b498028b
Daniel Garcia Campoy <daniel.garcia.c.alu@iescampanillas.com>
2024-12-13 13:06:39 +0100

    diseño register y login hecho

--- loginForm.php
+++ loginForm.php
@@ -1,31 +1,48 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Login</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>Iniciar Sesión</h1>
-    <form action="login.php" method="POST">
-        <label for="telefono">Teléfono:</label>
-        <input type="number" id="telefono" name="telefono" required>
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Ingresar</button>
-    </form>
-    <a href="registerForm.php"><button>Registrarse</button></a>
-    <?php if (isset($_GET['success'])): ?>
-        <?php if ($_GET['success'] == 1): ?>
-            <p style="color: green;">Cuenta creada con éxito, ahora inicie sesión</p>
-        <?php endif; ?>
-    <?php endif; ?>
-    <?php if (isset($_GET['error'])): ?>
-        <?php if ($_GET['error'] == 1): ?>
-            <p style="color: red;">Teléfono o contraseña introducida incorrecta</p>
-        <?php endif; ?>
-    <?php endif; ?>
+    <header class="index">
+        <div class="logo">
+            <img src="#" alt="logo">
+            <h1>Wasap 4</h1>
+        </div>
+    </header>
+    <main class="login">
+        <div class="container login">
+            <h1>Iniciar Sesión</h1>
+            <form action="login.php" method="POST">
+                <label for="telefono">Teléfono:</label>
+                <input type="number" id="telefono" name="telefono" required>
+                <br>
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" name="password" required>
+                <br>
+                <button type="submit">Ingresar</button>
+            </form>
+            <div class="registrarse">
+                <p>¿No tienes cuenta?</p>
+                <a href="registerForm.php"><button class="registrarse">Registrarse</button></a>
+            </div>
+            <?php if (isset($_GET['success'])): ?>
+                <?php if ($_GET['success'] == 1): ?>
+                    <p style="color: black; font-weight: bold">Cuenta creada con éxito, ahora inicie sesión</p>
+                <?php endif; ?>
+            <?php endif; ?>
+            <?php if (isset($_GET['error'])): ?>
+                <?php if ($_GET['error'] == 1): ?>
+                    <p style="color: red;">Teléfono o contraseña introducida incorrecta</p>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </main>
 </body>
+
 </html>
--- registerForm.php
+++ registerForm.php
@@ -1,26 +1,43 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registro</title>
+    <link rel="stylesheet" href="style.css">
 </head>
+
 <body>
-    <h1>Registro de Usuario</h1>
-    <form action="register.php" method="post">
-        <label for="telefono">Teléfono:</label>
-        <input type="text" id="telefono" name="telefono" required>
-        <br>
-        <label for="password">Contraseña:</label>
-        <input type="password" id="password" name="password" required>
-        <br>
-        <button type="submit">Registrarse</button>
-    </form>
-    <p>¿Ya tienes una cuenta? <a href="loginForm.php">Inicia sesión aquí</a>.</p>
-    <?php if (isset($_GET['error'])): ?>
-        <?php if ($_GET['error'] == 1): ?>
-            <p style="color: red;">El teléfono ya está registrado. Intenta con otro.</p>
-        <?php endif; ?>
-    <?php endif; ?>
+    <header class="index">
+        <div class="logo">
+            <img src="#" alt="logo">
+            <h1>Wasap 4</h1>
+        </div>
+    </header>
+    <main class="login">
+        <div class="container login">
+            <h1>Registro de Usuario</h1>
+            <form action="register.php" method="post">
+                <label for="telefono">Teléfono:</label>
+                <input type="number" id="telefono" name="telefono" required>
+                <br>
+                <label for="password">Contraseña:</label>
+                <input type="password" id="password" name="password" required>
+                <br>
+                <button type="submit">Registrarse</button>
+            </form>
+            <div class="registrarse">
+                <p>¿Ya tienes una cuenta?</p>
+                <a href="loginForm.php"><button class="registrarse">Inicia sesión</button></a>
+            </div>
+            <?php if (isset($_GET['error'])): ?>
+                <?php if ($_GET['error'] == 1): ?>
+                    <p style="color: red;">El teléfono ya está registrado. Intenta con otro.</p>
+                <?php endif; ?>
+            <?php endif; ?>
+        </div>
+    </main>
 </body>
+
 </html>
